9fdcadc843c33b23d904f818b68d3a2c6de890f7
osheafm <osheafm@s.dcsdk12.org>
2021-02-19 11:28:50 -0700

    Initial Commit

--- app/src/main/java/com/example/whackaworm/MainActivity.java
+++ app/src/main/java/com/example/whackaworm/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.whackaworm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageViewPlay = (ImageView) findViewById(R.id.ImagePlay);
+        imageViewPlay.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(getApplicationContext(), LevelOneActivity.class);
+               startActivity(intent);
+
+                return true;
+            }
+        });
     }
 }
