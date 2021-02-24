54125c1e1d5e2cc7e1ec96f7b7c757bbcc39be5e
osheafm <osheafm@s.dcsdk12.org>
2021-02-24 11:19:46 -0700

    Initial Commit

--- app/src/main/java/com/example/whackaworm/LevelOneActivity.java
+++ app/src/main/java/com/example/whackaworm/LevelOneActivity.java
@@ -3,27 +3,30 @@ package com.example.whackaworm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LevelOneActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_one);
+        textView = findViewById(R.id.timerTextView);
 
-        ImageView imageViewPlay = (ImageView) findViewById(R.id.imageView3);
-        imageViewPlay.setOnTouchListener(new View.OnTouchListener() {
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                // TODO Auto-generated method stub
-                Log.i("imageviewandontouchlistener", "playImageView onTouch");
-                return false;
+        new CountDownTimer(50000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                long sec = (millisUntilFinished / 1000) ;
+                textView.setText(sec+"");
             }
-        });
+            // When the task is over it will print 00:00:00 there
+            public void onFinish() {
+                textView.setText("0");
+            }
+        }.start();
     }
 }
