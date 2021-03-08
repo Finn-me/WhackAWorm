2e5ab368bdf77e87f3fab6fcaf824fdccd3efb6d
osheafm <osheafm@s.dcsdk12.org>
2021-03-08 11:30:13 -0700

    Initial Commit

--- app/src/main/java/com/example/whackaworm/LevelOneActivity.java
+++ app/src/main/java/com/example/whackaworm/LevelOneActivity.java
@@ -16,13 +16,15 @@ import java.util.Random;
 
 public class LevelOneActivity extends AppCompatActivity {
     TextView textView;
+    TextView ScoreView;
 ImageView[] Worms = new ImageView[8];
-
+    int score = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_one);
         textView = findViewById(R.id.timerTextView);
+        ScoreView = findViewById(R.id.ScoreView);
 
 
 
@@ -37,49 +39,73 @@ ImageView[] Worms = new ImageView[8];
 
         Worm1.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
+                Worm1.setVisibility(View.INVISIBLE);
                 Toast.makeText(getApplicationContext(),"worm 1 was touched", Toast.LENGTH_SHORT).show();
+               score = score + 1;
+                ScoreView.setText(score+"");
                 return false;
             }
         });
         Worm2.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
+                Worm2.setVisibility(View.INVISIBLE);
                 Toast.makeText(getApplicationContext(),"worm 2 was touched", Toast.LENGTH_SHORT).show();
+                score = score + 1;
+                ScoreView.setText(score+"");
                 return false;
             }
         });
         Worm3.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
+                Worm3.setVisibility(View.INVISIBLE);
                 Toast.makeText(getApplicationContext(),"worm 3 was touched", Toast.LENGTH_SHORT).show();
+                score = score + 1;
+                ScoreView.setText(score+"");
                 return false;
             }
         });
         Worm4.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
+                Worm4.setVisibility(View.INVISIBLE);
                 Toast.makeText(getApplicationContext(),"worm 4 was touched", Toast.LENGTH_SHORT).show();
+                score = score + 1;
+                ScoreView.setText(score+"");
                 return false;
             }
         });
         Worm5.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
+                Worm5.setVisibility(View.INVISIBLE);
                 Toast.makeText(getApplicationContext(),"worm 5 was touched", Toast.LENGTH_SHORT).show();
+                score = score + 1;
+                ScoreView.setText(score+"");
                 return false;
             }
         });
         Worm6.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
+                Worm6.setVisibility(View.INVISIBLE);
                 Toast.makeText(getApplicationContext(),"worm 6 was touched", Toast.LENGTH_SHORT).show();
+                score = score + 1;
+                ScoreView.setText(score+"");
                 return false;
             }
         });
         Worm7.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
+                Worm7.setVisibility(View.INVISIBLE);
                 Toast.makeText(getApplicationContext(),"worm 7 was touched", Toast.LENGTH_SHORT).show();
+                score = score + 1;
+                ScoreView.setText(score+"");
                 return false;
             }
         });
         Worm8.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
+                Worm8.setVisibility(View.INVISIBLE);
                 Toast.makeText(getApplicationContext(),"worm 8 was touched", Toast.LENGTH_SHORT).show();
+                score = score + 1;
+                ScoreView.setText(score+"");
                 return false;
             }
         });
@@ -101,33 +127,39 @@ ImageView[] Worms = new ImageView[8];
         Worms[5] = Worm6;
         Worms[6] = Worm7;
         Worms[7] = Worm8;
-        new CountDownTimer(50000, 1000) {
+        CountDownTimer wormTimer = new CountDownTimer(3000, 3000) {
+            //Declare a global variable here
+            Random random = new Random();
+            int randNum;
+
             public void onTick(long millisUntilFinished) {
-                long sec = (millisUntilFinished / 1000) ;
+                //Get the actual random value here
+                randNum = random.nextInt(8);
+                Worms[randNum].setVisibility(View.VISIBLE);
+            }
+
+            public void onFinish() {
+                Worms[randNum].setVisibility(View.INVISIBLE);
+
+                start();
+
+
+            }
+        }.start();
+
+        CountDownTimer mainTimer = new CountDownTimer(30000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                long sec = millisUntilFinished / 1000;
                 textView.setText(sec+"");
             }
             // When the task is over it will print 00:00:00 there
             public void onFinish() {
+                wormTimer.cancel();
                 textView.setText("0");
             }
-        }
-        .start();
+        }.start();
 
-        new CountDownTimer(3000, 3000) {
-            Random random = new Random();
-            int randNum;
-            public void onTick(long millisUntilFinished) {
-                long sec = (millisUntilFinished / 1000) ;
-               randNum = random.nextInt(8);
-                Worms[randNum].setVisibility(View.VISIBLE);
 
-            }
-            // When the task is over it will print 00:00:00 there
-            public void onFinish() {
-                Worms[randNum].setVisibility(View.INVISIBLE);
-            start();
-            }
-        } .start();
 
     }
 }
